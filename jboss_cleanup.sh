991a934e68929e63702ef0c3910cf359c3136625
Samuel Lidén Borell <samuel@kodafritt.se>
2014-11-18 17:23:05 +0100

    Script to clean up JBoss temp files

---
+++ jboss_cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This script cleans up temporary files from JBoss. Put it in your JBoss root directory and run it from there.
+# It is normal for it to show some error messages due to already deleted files (e.g. if ran several times in a row).
+
+rm -r standalone/deployments/ejbca.ear* standalone/data/timer-service-data/* standalone/data/wsdl/ejbca.ear/ standalone/tmp/vfs/* standalone/tmp/work/jboss.web/default-host/*
+
