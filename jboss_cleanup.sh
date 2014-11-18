c78a773939a99dfb8b1d439345b8bc6d75a18497
Samuel Lidén Borell <samuel@kodafritt.se>
2014-11-18 17:26:00 +0100

    Mention that the JBoss cleanup script is for JBoss 7 and EAP 6

--- jboss_cleanup.sh
+++ jboss_cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script cleans up temporary files from JBoss. Put it in your JBoss root directory and run it from there.
+# This script cleans up temporary files from JBoss 7 / EAP 6. Put it in your JBoss root directory and run it from there.
 # It is normal for it to show some error messages due to already deleted files (e.g. if ran several times in a row).
 
 rm -r standalone/deployments/ejbca.ear* standalone/data/timer-service-data/* standalone/data/wsdl/ejbca.ear/ standalone/tmp/vfs/* standalone/tmp/work/jboss.web/default-host/*
