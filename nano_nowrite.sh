d96aa27c225b1a31778495818ccce8a7b062341a
Samuel Lidén Borell <samuel@kodafritt.se>
2016-09-27 20:34:20 +0200

    Fix nano_nowrite script to handle new files correctly

--- nano_nowrite.sh
+++ nano_nowrite.sh
@@ -1,7 +1,23 @@
 #!/bin/sh -e
 
-if [ $# = 1 -a -r "$1" -a ! -w "$1" ]; then
-    echo "WARNING!!! File is not writeable: $1"
-    read REPLY
+msg() {
+    printf '\033[1;31m%s\033[0m\n' "$1"
+}
+
+if [ $# = 1 ]; then
+    # Check if it's an existing file
+    if [ -r "$1" ]; then
+        if [ ! -w "$1" ]; then
+            msg "WARNING!!! File is not writeable: $1"
+            read REPLY
+        fi
+    else
+        # It's a new file, check if the containing directory is writable
+        dir="$(dirname "$1")"
+        if [ ! -w "$dir" ]; then
+            msg "WARNING!!! Can't write in directory: $dir"
+            read REPLY
+        fi
+    fi
 fi
 nano "$@"
