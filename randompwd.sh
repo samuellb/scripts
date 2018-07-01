c28a8d3c3b35302f2dfc0d93947bf9b76ea48dc5
Samuel Lidén Borell <samuel@kodafritt.se>
2018-07-01 14:51:14 +0200

    Randompwd - Utility to generate random passwords

---
+++ randompwd.sh
@@ -0,0 +1,47 @@
+#!/bin/sh -eu
+#
+#  randompwd -- Generates a random password
+#
+#  Copyright (c) 2018 Samuel Lidén Borell <samuel@kodafritt.se>
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+#
+
+length="${1:-24}"
+case "$length" in
+--help|-h)
+    cat >&2 <<EOF
+Usage: $0 LENGTH
+
+Generates a password consisting of the characters A-Z a-z 0-9
+using /dev/urandom as a randomness source.
+
+EOF
+    exit;;
+-*)
+    cat >&2 <<EOF
+Usage: $0 LENGTH
+Type $0 --help for help.
+EOF
+    exit 2
+    ;;
+esac
+
+tr -c -d 'a-zA-Z0-9' < /dev/urandom  | dd bs=1 count="$length" 2> /dev/null; echo
+
