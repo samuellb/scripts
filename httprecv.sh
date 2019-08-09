14f19d07b86c2347d297a5312d0d467d49a6ea79
Samuel Lidén Borell <samuel@kodafritt.se>
2019-08-09 17:42:39 +0200

    Code cleanup

--- httprecv.sh
+++ httprecv.sh
@@ -139,8 +139,6 @@ read_multipart_boundary() {
     boundary="$1"
     read line
     line="${line%$cr}"
-#echo "line: <$line>" >&2
-#echo "mpb:  <--$boundary>" >&2
     if [ "$line" != "--$boundary" ]; then
         resp_message 400 "Bad request"
         echo "Error: Unexpected multipart boundary from client." >&2
@@ -185,14 +183,12 @@ content_type_boundary=""
 content_length=""
 while read hdr; do
     hdr="${hdr%$cr}"
-#echo "Read header: $hdr" >&2 # TODO remove
     [ -n "$hdr" ] || break
     hdrlower=$(printf %s "$hdr" | tr '[:upper:]' '[:lower:]')
     hdrname=$(printf %s "$hdrlower" | cut -d ':' -f 1 | tr -s " \t")
     hdrname=${hdrname% }
     hdrvalue=$(printf %s "$hdr" | cut -d ':' -f 2- | tr -s " \t")
     hdrvalue=${hdrvalue# }
-#echo "Read header: <$hdrname> <$hdrvalue>" >&2 # TODO remove
     case "$hdrname" in
         content-length)
             tmp=$(printf %s "$hdrvalue" | tr -cd 0123456789)
@@ -206,7 +202,6 @@ while read hdr; do
             while true; do
                 remaining=$(printf %s "$remaining" | cut -sd ';' -f 2- | tr -s ' \t')
                 remaining=${remaining# }
-#                echo "remaining=<$remaining>" >&2
                 [ -n "$remaining" ] || break
                 if [ "${remaining#boundary=}" != "$remaining" ]; then
                     content_type_boundary=$(printf %s "${remaining#boundary=}" | cut -d ';' -f 1 | tr -s ' \t')
@@ -215,7 +210,6 @@ while read hdr; do
             unset remaining;;
     esac
 done
-#echo "Done reading headers... CL=<$content_length> CT=<$content_type> CTB=<$content_type_boundary>" >&2 # TODO remove
 
 if [ "$method" = HEAD ]; then
     cat <<EOF
@@ -234,34 +228,26 @@ if [ "$method" = POST -a "$content_length" = 0 ]; then
 elif [ "$method" = POST ]; then
     log INFO "Receiving file data ($content_length bytes)" # header is sanitized, so this is OK
     uploadstatus='Upload failed'
-    #if [ -z "$content_length" ]; then
-    #    resp_message 411 'Length Required'
-    #    exit
-    #fi
     if [ "$content_type" != multipart/form-data ]; then
         resp_message 415 'Unsupported form encoding'
         exit
     fi
     uploadstatus='Error: Upload was malformed'
     if [ -n "$content_length" ]; then
-        #dd of=upload.mime bs=1 count="$content_length"
         dd bs=1 count="$content_length" 2>/dev/null
     else
-        #cat > "$upload.mime"
         cat
     fi | {
         read_multipart_boundary "$content_type_boundary"
         part_length=""
         while read line; do
             line="${line%$cr}"
-#echo "Multipart line: <$line>" >&2 # TODO remove
             [ -n "$line" ] || break
             hdrlower=$(printf %s "$hdr" | tr '[:upper:]' '[:lower:]')
             hdrname=$(printf %s "$hdrlower" | cut -d ':' -f 1 | tr -s " \t")
             hdrname=${hdrname% }
             hdrvalue=$(printf %s "$hdr" | cut -d ':' -f 2- | tr -s " \t")
             hdrvalue=${hdrvalue# }
-#echo "Part header: <$hdrname> <$hdrvalue>" >&2 # TODO remove
             case "$hdrname" in
                 content-length)
                     tmp=$(printf %s "$hdrvalue" | tr -cd 0123456789)
@@ -280,8 +266,6 @@ elif [ "$method" = POST ]; then
 #        else
 #            dd of=upload.bin bs=1 count="$part_length"
 #        fi
-#        read line
-#echo "got line: $line" >&2
 #        read_multipart_boundary "$content_type_boundary--"
         boundary_length=$(printf '%s' "RN--$boundary--RN" | wc -c) # include space for CR-LF before and after
         head -c -"$boundary_length" > upload.bin # FIXME this does not check the final multipart boundary
