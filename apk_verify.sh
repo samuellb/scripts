9282cc298d4bbf2c8313a1b357d937735bf93c5f
Samuel Lidén Borell <samuel@kodafritt.se>
2021-06-30 16:00:25 +0200

    apk_verify: [SECURITY FIX] Explicitly specify name of certificate file
    This commit adds explicit specification of the certificate file name.
    Without this change, a tampered-with APK file could possibly be accepted
    if it contains the trusted certificate file, but is actually signed by
    another file. (I haven't tested with a such APK file, and I don't know if
    Android would accept it, though).

--- apk_verify.sh
+++ apk_verify.sh
@@ -116,7 +116,7 @@ while [ $# -ge 1 ]; do
         failure "$1" "jarsigner command failed"
         ok=0
     }
-    unsigned=$(jarsigner -verify -certs -verbose "$1" | grep -E '^[a-z0-9 ]{10,10}[0-9]' | grep -vE '^sm ' | cut -c '-3,42-' |
+    unsigned=$(jarsigner -verify -certs -verbose -sigfile "$namebase" -sigalg "$sigalg" "$1" | grep -E '^[a-z0-9 ]{10,10}[0-9]' | grep -vE '^sm ' | cut -c '-3,42-' |
         grep -cvE "^(s +META-INF/MANIFEST\\.MF| +META-INF/$namebase\\.($sigalg|SF))\$" || true)
     if [ "$unsigned" != 0 ]; then
         failure "$1" "There are $unsigned unsigned files:"
